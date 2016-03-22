9c0e020a89226727541a0394d56f3dc1cd899e06
xkbeyer <xkbeyer@gmail.com>
2016-03-22 16:28:05 +0100

    Add cli option for import paths to search for.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,66 @@
 #include "main.h"
 
 #include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <cassert>
 #include "config.h"
 #include "CodeGenContext.h"
 #include "AstNode.h"
+#include "GetOpt.h"
 
 extern int yyparse();
 extern int yylex_destroy();
 extern FILE* yyin;
 extern liquid::Block* programBlock;
 extern std::stack<std::string> fileNames;
+extern std::vector<std::string> libPaths;
 
+void usage();
 
 int main(int argc, char **argv)
 {
     std::string fileName;
     std::cout << "liquid version " << MAJOR_VER << "." << MINOR_VER << "." << REVISION_VER << "\n";
-    if( argc == 2 ) {
-        fileName = argv[1] ;
-    } else {
+    if( argc == 1 ) {
         fileName = "./test_full.liq";
     }
+
+    libPaths.push_back("./"); // current path
+
+    GetOpt getopt(argc, argv, "hi:");
+    for( auto opt : getopt ) {
+       switch( opt ) {
+          case 'i':
+          {
+             std::stringstream ss(getopt.get());
+             std::string item;
+             while( std::getline(ss, item, ';') ) {
+                std::replace(std::begin(item), std::end(item), '\\', '/');
+                if( item[item.size()] != '/' )
+                   item += '/';
+                libPaths.push_back(item);
+             }
+          }
+             break;
+          case 'h':
+          default:
+             usage();
+             return 1;
+       }
+    }
+    
+    auto files = getopt.getRemainingArguments();
+    assert(files.size() == 1);
+    fileName = files[0]; // Currently only one file is supported.
+
     yyin = fopen(fileName.c_str(), "r+") ;
     if ( yyin == nullptr )
     {
        std::cout << "File "<< fileName << "not found. Abort" << std::endl;
        return -1;
     }
+
     fileNames.push(""); // Add the empty file name after last EOF.
     fileNames.push(fileName); // Add the top level file name.
     yyparse();
@@ -50,4 +82,11 @@ int main(int argc, char **argv)
     delete programBlock;
     yylex_destroy();
     return 0;
+}
+
+void usage()
+{
+   std::cout << "Usage:\n";
+   std::cout << "liq filename -i path1;path2\n";
+   std::cout << "\t-i semicolon separated list of import paths where additional liquid files are located.\n";
 }
