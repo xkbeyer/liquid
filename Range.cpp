44510201e6cbb21dd1379e67dff0b9f228bb8625
xkbeyer <xkbeyer@gmail.com>
2016-07-10 11:13:03 +0200

    Codegen for range, but only a simple case works.
    It looks like that I have to rethink the way how list/array
    is implemented :(

--- Range.cpp
+++ Range.cpp
@@ -1,5 +1,6 @@
 #include "AstNode.h"
 #include "CodeGenContext.h"
+#include "parser.hpp"
 
 using namespace std;
 using namespace llvm;
@@ -8,7 +9,44 @@ namespace liquid {
 
 llvm::Value* Range::codeGen(CodeGenContext& context) 
 {
-   return nullptr; 
+   // var l = [lhs]
+   // var n = lhs
+   // while n < end 
+   //    n += 1
+   //    l << n
+   // return l
+   
+   YYLTYPE loc = { 0,0,0,0 };
+   Block tmp_code;
+   auto rc = context.findVariable("tmp_l");
+   if( rc ) {
+      context.deleteVariable("tmp_l");
+   }
+   rc = context.findVariable("tmp_n");
+   if( rc ) {
+      context.deleteVariable("tmp_n");
+   }
+   ExpressionList exprs;
+   exprs.push_back(begin);
+   auto l = new List( &exprs, loc);
+   auto vardecl = new VariableDeclarationDeduce(new Identifier("tmp_l", loc), l, loc);
+   tmp_code.statements.push_back(vardecl);
+
+   auto vardecl_n = new VariableDeclarationDeduce(new Identifier("tmp_n", loc), this->begin, loc);
+   tmp_code.statements.push_back(vardecl_n);
+   
+   auto while_block = new Block();
+   auto assgn = new Assignment(new Identifier("tmp_n", loc), new BinaryOp(new Identifier("tmp_n", loc), TPLUS, new Integer(1), loc), loc);
+   auto l_plus_n = new ListAddElement(new Identifier("tmp_l", loc), new Identifier("tmp_n", loc), loc);
+   while_block->statements.push_back(assgn);
+   while_block->statements.push_back(l_plus_n);
+   auto cond = new CompOperator(new Identifier("tmp_n", loc), TCLE, this->end);
+   auto wl = new WhileLoop(cond, while_block);
+   tmp_code.statements.push_back(wl);
+   auto v = tmp_code.codeGen(context);
+   context.getModule()->dump();
+   rc = context.findVariable("tmp_l");
+   return rc;
 }
 
 
