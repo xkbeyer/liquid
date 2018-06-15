04ea87d28e5207445c5e3100b030c3836fc93493
xkbeyer <xkbeyer@gmail.com>
2018-06-15 15:31:00 +0200

    Fix loading external functions (built-ins).
    Fixes #4

--- CodeGenContext.cpp
+++ CodeGenContext.cpp
@@ -37,20 +37,17 @@ using namespace llvm;
 #endif
 #endif
 
-
-
-namespace liquid {
-
-CodeGenContext::CodeGenContext(std::ostream& outs)
-: outs(outs)
+namespace liquid
 {
-    llvm::InitializeNativeTarget();
-    llvm::InitializeNativeTargetAsmParser();
-    llvm::InitializeNativeTargetAsmPrinter();
-    module = new llvm::Module("liquid", llvmContext);
+CodeGenContext::CodeGenContext(std::ostream& outs) : outs(outs)
+{
+   llvm::InitializeNativeTarget();
+   llvm::InitializeNativeTargetAsmParser();
+   llvm::InitializeNativeTargetAsmPrinter();
+   module = new llvm::Module("liquid", llvmContext);
 }
 
-/*! Setup up the built in function:
+   /*! Setup up the built in function:
     * - printvalue
     * - printdouble
     * - sin
@@ -61,103 +58,112 @@ CodeGenContext::CodeGenContext(std::ostream& outs)
 #define MAKE_LLVM_EXTERNAL_NAME(a) #a
 void CodeGenContext::setupBuiltIns()
 {
-    auto intType = getGenericIntegerType();
+   auto intType = getGenericIntegerType();
 
-    std::vector<Type *> argTypesOneInt(1, intType);
-    FunctionType * ft = FunctionType::get(intType, argTypesOneInt, false);
-    Function * f = Function::Create(ft, Function::ExternalLinkage,MAKE_LLVM_EXTERNAL_NAME(printvalue),getModule());
-    Function::arg_iterator i = f->arg_begin();
-    if( i != f->arg_end() )
-        i->setName("val");
+   std::vector<Type*>     argTypesOneInt(1, intType);
+   FunctionType*          ft = FunctionType::get(intType, argTypesOneInt, false);
+   Function*              f  = Function::Create(ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(printvalue), getModule());
+   Function::arg_iterator i  = f->arg_begin();
+   if (i != f->arg_end())
+      i->setName("val");
+   builtins.push_back({f, (void*)printvalue});
 
-    std::vector<Type *> argTypesOneDouble(1, Type::getDoubleTy(getGlobalContext()));
-    ft = FunctionType::get(Type::getDoubleTy(getGlobalContext()), argTypesOneDouble,false);
-    f = Function::Create( ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(printdouble), getModule() );
-    i = f->arg_begin();
-    if( i != f->arg_end() )
-        i->setName("val");
+   std::vector<Type*> argTypesOneDouble(1, Type::getDoubleTy(getGlobalContext()));
+   ft = FunctionType::get(Type::getDoubleTy(getGlobalContext()), argTypesOneDouble, false);
+   f  = Function::Create(ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(printdouble), getModule());
+   i  = f->arg_begin();
+   if (i != f->arg_end())
+      i->setName("val");
+   builtins.push_back({f, (void*)printdouble});
 
-    ft = FunctionType::get(Type::getDoubleTy(getGlobalContext()), argTypesOneDouble,false);
-    f = Function::Create( ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(sin),getModule());
-    i = f->arg_begin();
-    if( i != f->arg_end() )
-        i->setName("val");
+   // ft = FunctionType::get(Type::getDoubleTy(getGlobalContext()), argTypesOneDouble,false);
+   // f = Function::Create( ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(sin),getModule());
+   // i = f->arg_begin();
+   // if( i != f->arg_end() )
+   //    i->setName("val");
+   //builtins.push_back({f, (void*)sin});
 
-    std::vector<Type *>argTypesInt8Ptr(1, Type::getInt8PtrTy(getGlobalContext()));
-    ft = FunctionType::get(Type::getVoidTy(getGlobalContext()),argTypesInt8Ptr, true);
-    f = Function::Create( ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(display), getModule() );
-    i = f->arg_begin();
-    if( i != f->arg_end() )
-        i->setName("format_str");
+   std::vector<Type*> argTypesInt8Ptr(1, Type::getInt8PtrTy(getGlobalContext()));
+   ft = FunctionType::get(Type::getVoidTy(getGlobalContext()), argTypesInt8Ptr, true);
+   f  = Function::Create(ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(display), getModule());
+   i  = f->arg_begin();
+   if (i != f->arg_end())
+      i->setName("format_str");
+   builtins.push_back({f, (void*)display});
 
-    ft = FunctionType::get(Type::getVoidTy(getGlobalContext()),argTypesInt8Ptr, true);
-    f = Function::Create( ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(displayln),getModule());
-    i = f->arg_begin();
-    if( i != f->arg_end() )
-        i->setName("format_str");
-    
+   ft = FunctionType::get(Type::getVoidTy(getGlobalContext()), argTypesInt8Ptr, true);
+   f  = Function::Create(ft, Function::ExternalLinkage, MAKE_LLVM_EXTERNAL_NAME(displayln), getModule());
+   i  = f->arg_begin();
+   if (i != f->arg_end())
+      i->setName("format_str");
+   builtins.push_back({f, (void*)displayln});
 }
 
 /*! Compile the AST into a module */
 bool CodeGenContext::generateCode(Block& root)
 {
-    outs << "Generating code...\n";
+   outs << "Generating code...\n";
 
-    /* Create the top level interpreter function to call as entry */
-    vector<Type*> argTypes;
-    FunctionType *ftype = FunctionType::get(Type::getVoidTy(getGlobalContext()), argTypes, false);
-    mainFunction = Function::Create(ftype, GlobalValue::InternalLinkage, "main", getModule());
-    BasicBlock *bblock = BasicBlock::Create(getGlobalContext(), "entry", mainFunction, 0);
-    setupBuiltIns();
-    /* Push a new variable/block context */
-    newScope(bblock);
-    root.codeGen(*this); /* emit byte code for the top level block */
-    if(errors) {
-       outs << "Compilation error(s). Abort.\n";
-       return false;
-    }
-    if( currentBlock()->getTerminator() == nullptr ) {
-        ReturnInst::Create(getGlobalContext(),0, currentBlock());
-    }
-    endScope();
+   /* Create the top level interpreter function to call as entry */
+   vector<Type*> argTypes;
+   FunctionType* ftype = FunctionType::get(Type::getVoidTy(getGlobalContext()), argTypes, false);
+   mainFunction        = Function::Create(ftype, GlobalValue::InternalLinkage, "main", getModule());
+   BasicBlock* bblock  = BasicBlock::Create(getGlobalContext(), "entry", mainFunction, 0);
+   setupBuiltIns();
+   /* Push a new variable/block context */
+   newScope(bblock);
+   root.codeGen(*this); /* emit byte code for the top level block */
+   if (errors) {
+      outs << "Compilation error(s). Abort.\n";
+      return false;
+   }
+   if (currentBlock()->getTerminator() == nullptr) {
+      ReturnInst::Create(getGlobalContext(), 0, currentBlock());
+   }
+   endScope();
 
-    outs << "Code is generated.\n";
+   outs << "Code is generated.\n";
 
-    outs << "verifying... ";
-    if( verifyModule(*getModule()) ) {
-       outs << ": Error constructing function!\n";
-       #if !defined(LLVM_NO_DUMP)
-       module->dump();
-       #endif
-       return false;
-    }
-    outs << "done.\n";
+   outs << "verifying... ";
+   if (verifyModule(*getModule())) {
+      outs << ": Error constructing function!\n";
+#if !defined(LLVM_NO_DUMP)
+      module->dump();
+#endif
+      return false;
+   }
+   outs << "done.\n";
 
-    if( !debug ) {
-       optimize();
-    }
-    #if !defined(LLVM_NO_DUMP) // Only the debug build of LLVM has a dump() method.
-    /* Print the byte code in a human-readable format
-     *     to see if our program compiled properly
-     */
-    if( verbose )
-       module->dump();
-    #endif
-    return true;
+   if (!debug) {
+      optimize();
+   }
+#if !defined(LLVM_NO_DUMP) // Only the debug build of LLVM has a dump() method.
+   /* Print the byte code in a human-readable format
+    *     to see if our program compiled properly
+    */
+   if (verbose)
+      module->dump();
+#endif
+   return true;
 }
 
 /*! Executes the AST by running the main function */
-GenericValue CodeGenContext::runCode() {
-    outs << "Running code...\n";
-    std::string err;
-    ExecutionEngine *ee = EngineBuilder( std::unique_ptr<Module>( module ) ).setErrorStr( &err ).setEngineKind( EngineKind::JIT ).create();
-    assert(ee);
-    ee->finalizeObject();
-    vector<GenericValue> noargs;
-    GenericValue v = ee->runFunction(mainFunction, noargs);
-    outs << "Code was run.\n";
-    delete ee;
-    return v;
+GenericValue CodeGenContext::runCode()
+{
+   outs << "Running code...\n";
+   std::string      err;
+   ExecutionEngine* ee = EngineBuilder(std::unique_ptr<Module>(module)).setErrorStr(&err).setEngineKind(EngineKind::JIT).create();
+   assert(ee);
+   for (auto info : builtins) {
+      ee->addGlobalMapping(info.f, info.addr);
+   }
+
+   ee->finalizeObject();
+   vector<GenericValue> noargs;
+   GenericValue         v = ee->runFunction(mainFunction, noargs);
+   outs << "Code was run.\n";
+   delete ee;
+   return v;
 }
 
 void CodeGenContext::printCodeGeneration(class Block& root, std::ostream& outs)
@@ -171,63 +177,63 @@ void CodeGenContext::optimize()
 {
    outs << "Optimize code...\n";
    legacy::FunctionPassManager fpm(getModule());
-    PassManagerBuilder builder;
-    builder.OptLevel = 3;
-    builder.populateFunctionPassManager(fpm);
-    for ( auto& fn : getModule()->getFunctionList() )
-    {
-        fpm.run( fn );
-    }
-    fpm.run(*mainFunction);
+   PassManagerBuilder          builder;
+   builder.OptLevel = 3;
+   builder.populateFunctionPassManager(fpm);
+   for (auto& fn : getModule()->getFunctionList()) {
+      fpm.run(fn);
+   }
+   fpm.run(*mainFunction);
 }
 
 void CodeGenContext::newScope(BasicBlock* bb, ScopeType scopeType)
 {
-    currentScopeType = scopeType;
-    if( bb == nullptr) {
-        bb = llvm::BasicBlock::Create(getGlobalContext(),"scope");
-    }
-    codeBlocks.push_front(new CodeGenBlock(bb));
+   currentScopeType = scopeType;
+   if (bb == nullptr) {
+      bb = llvm::BasicBlock::Create(getGlobalContext(), "scope");
+   }
+   codeBlocks.push_front(new CodeGenBlock(bb));
 }
 
-void CodeGenContext::endScope() {
-    CodeGenBlock* top = codeBlocks.front();
-    codeBlocks.pop_front();
-    delete top;
-    currentScopeType = ScopeType::CodeBlock;
+void CodeGenContext::endScope()
+{
+   CodeGenBlock* top = codeBlocks.front();
+   codeBlocks.pop_front();
+   delete top;
+   currentScopeType = ScopeType::CodeBlock;
 }
 
 /*! Searches a variable name in all known locals of the current code block.
  * If not found it looks in the current class block, too.
  * \param[in] varName variable name
- * \return The alloca instruction 
+ * \return The alloca instruction
  */
 AllocaInst* CodeGenContext::findVariable(std::string varName)
 {
-    ValueNames& names =  locals();
-    if(names.find(varName) != names.end() ) {
-        return names[varName];
-    }
-    if( self ) {
-        ValueNames& names = self->getValueNames();
-        if(names.find(varName) != names.end() ) {
-            return names[varName];
-        }
-    }
-    return nullptr;
+   ValueNames& names = locals();
+   if (names.find(varName) != names.end()) {
+      return names[varName];
+   }
+   if (self) {
+      ValueNames& names = self->getValueNames();
+      if (names.find(varName) != names.end()) {
+         return names[varName];
+      }
+   }
+   return nullptr;
 }
 
 /*! Deletes a variable name in all known locals of the current code block.
- * 
+ *
  * \param[in] varName variable name
  */
 void CodeGenContext::deleteVariable(std::string varName)
 {
-   ValueNames& names = locals();
-   auto& typeMap = codeBlocks.front()->getTypeMap();
-   if( names.find(varName) != names.end() ) {
+   ValueNames& names   = locals();
+   auto&       typeMap = codeBlocks.front()->getTypeMap();
+   if (names.find(varName) != names.end()) {
       names.erase(varName);
-      if( typeMap.count(varName) ) {
+      if (typeMap.count(varName)) {
          typeMap.erase(varName);
       }
    }
@@ -241,12 +247,12 @@ void CodeGenContext::deleteVariable(std::string varName)
 void CodeGenContext::renameVariable(std::string oldVarName, std::string newVarName)
 {
    ValueNames& names = locals();
-   if( names.find(oldVarName) != names.end() ) {
+   if (names.find(oldVarName) != names.end()) {
       auto value = names[oldVarName];
       names.erase(oldVarName);
       names[newVarName] = value;
-      auto& typeMap = codeBlocks.front()->getTypeMap();
-      if( typeMap.count(oldVarName) ) {
+      auto& typeMap     = codeBlocks.front()->getTypeMap();
+      if (typeMap.count(oldVarName)) {
          auto value = typeMap[oldVarName];
          typeMap.erase(oldVarName);
          typeMap[newVarName] = value;
@@ -255,18 +261,18 @@ void CodeGenContext::renameVariable(std::string oldVarName, std::string newVarNa
 }
 
 /*! Creates a new class block scope. */
-void CodeGenContext::newKlass(std::string  name)
+void CodeGenContext::newKlass(std::string name)
 {
-    self = codeBlocks.front();
-    klassName = name;
-    classAttributes[klassName] = KlassValueNames();
+   self                       = codeBlocks.front();
+   klassName                  = name;
+   classAttributes[klassName] = KlassValueNames();
 }
 
 /*! Closes a class block scope */
 void CodeGenContext::endKlass()
 {
-    self = nullptr;
-    klassName = "";
+   self      = nullptr;
+   klassName = "";
 }
 
 /*! Adds a class member variable name and its index to the list of the
@@ -274,12 +280,9 @@ void CodeGenContext::endKlass()
  * goes via the index into this structure.
  * \param[in] name Variable name of the current class.
  * \param[in] index into the llvm structure of this class.
- * 
+ *
  */
-void CodeGenContext::klassAddVariableAccess(std::string name, int index)
-{
-    classAttributes[klassName][name] = index;
-}
+void CodeGenContext::klassAddVariableAccess(std::string name, int index) { classAttributes[klassName][name] = index; }
 
 /*! Returns the load/getelementptr instruction to access a class member.
  * \param[in] klass The class name
@@ -289,22 +292,22 @@ void CodeGenContext::klassAddVariableAccess(std::string name, int index)
  * \remark Is used to access the elements via the alloca where the ptr to the ref of the class object is stored.
  *         Means that the ref is stored in a local (stack) variable.
  */
-Instruction * CodeGenContext::getKlassVarAccessInst(std::string klass, std::string name, AllocaInst* this_ptr)
+Instruction* CodeGenContext::getKlassVarAccessInst(std::string klass, std::string name, AllocaInst* this_ptr)
 {
-    assert( classAttributes.find(klass) != classAttributes.end());
-    int index = classAttributes[klass][name];
-    std::vector<Value*> ptr_indices;
-    ConstantInt* const_int32_0 = ConstantInt::get(getModule()->getContext(), APInt(32, 0));
-    ConstantInt* const_int32 = ConstantInt::get(getModule()->getContext(), APInt(32, index));
-    ptr_indices.push_back(const_int32_0);
-    ptr_indices.push_back(const_int32);
-    if( this_ptr->getType()->getElementType()->isPointerTy() ) {
-        // since the alloc is a ptr to ptr 
-        Value * val = new LoadInst(this_ptr, "", false, currentBlock());
-        return GetElementPtrInst::Create( val->getType()->getPointerElementType(), val, ptr_indices, "", currentBlock() );
-    }
-    Instruction* ptr = GetElementPtrInst::Create( this_ptr->getType()->getElementType(), this_ptr, ptr_indices, "", currentBlock() );
-    return ptr;
+   assert(classAttributes.find(klass) != classAttributes.end());
+   int                 index = classAttributes[klass][name];
+   std::vector<Value*> ptr_indices;
+   ConstantInt*        const_int32_0 = ConstantInt::get(getModule()->getContext(), APInt(32, 0));
+   ConstantInt*        const_int32   = ConstantInt::get(getModule()->getContext(), APInt(32, index));
+   ptr_indices.push_back(const_int32_0);
+   ptr_indices.push_back(const_int32);
+   if (this_ptr->getType()->getElementType()->isPointerTy()) {
+      // since the alloc is a ptr to ptr
+      Value* val = new LoadInst(this_ptr, "", false, currentBlock());
+      return GetElementPtrInst::Create(val->getType()->getPointerElementType(), val, ptr_indices, "", currentBlock());
+   }
+   Instruction* ptr = GetElementPtrInst::Create(this_ptr->getType()->getElementType(), this_ptr, ptr_indices, "", currentBlock());
+   return ptr;
 }
 
 /*! Get the type of a variable name.
@@ -313,68 +316,55 @@ Instruction * CodeGenContext::getKlassVarAccessInst(std::string klass, std::stri
  */
 std::string CodeGenContext::getType(std::string varName)
 {
-    if( varName == "self" ) {
-        return klassName;
-    }
-    if( codeBlocks.front()->getTypeMap().find(varName) != codeBlocks.front()->getTypeMap().end() ){
-        return codeBlocks.front()->getTypeMap()[varName];
-    }
-    return std::string("");
+   if (varName == "self") {
+      return klassName;
+   }
+   if (codeBlocks.front()->getTypeMap().find(varName) != codeBlocks.front()->getTypeMap().end()) {
+      return codeBlocks.front()->getTypeMap()[varName];
+   }
+   return std::string("");
 }
 
 /* Returns an LLVM type based on the identifier */
-Type* CodeGenContext::typeOf(const Identifier& type)
-{
-    return typeOf(type.getName());
-}
+Type* CodeGenContext::typeOf(const Identifier& type) { return typeOf(type.getName()); }
 
 Type* CodeGenContext::typeOf(const std::string name)
 {
-    if (name.compare("int") == 0) {
-        return getGenericIntegerType();
-    } else if (name.compare("double") == 0) {
-        return Type::getDoubleTy(getGlobalContext());
-    } else if (name.compare("string") == 0) {
-        return Type::getInt8PtrTy(getGlobalContext());
-    } else if (name.compare("boolean") == 0) {
-        return Type::getInt1Ty(getGlobalContext());
-    } else if (name.compare("void") == 0) {
-        return Type::getVoidTy(getGlobalContext());
-    }
-    llvm::Type* ty = getModule()->getTypeByName("class." + name);
-    if( ty != nullptr ) {
-        return ty;
-    }
-    return Type::getVoidTy(getGlobalContext());
-    
+   if (name.compare("int") == 0) {
+      return getGenericIntegerType();
+   } else if (name.compare("double") == 0) {
+      return Type::getDoubleTy(getGlobalContext());
+   } else if (name.compare("string") == 0) {
+      return Type::getInt8PtrTy(getGlobalContext());
+   } else if (name.compare("boolean") == 0) {
+      return Type::getInt1Ty(getGlobalContext());
+   } else if (name.compare("void") == 0) {
+      return Type::getVoidTy(getGlobalContext());
+   }
+   llvm::Type* ty = getModule()->getTypeByName("class." + name);
+   if (ty != nullptr) {
+      return ty;
+   }
+   return Type::getVoidTy(getGlobalContext());
 }
 
-void CodeGenContext::addKlassInitCode( std::string name, Assignment* assign )
-{
-    classInitCode[name].insert( assign );
-}
+void CodeGenContext::addKlassInitCode(std::string name, Assignment* assign) { classInitCode[name].insert(assign); }
 
-KlassInitCodeAssign& CodeGenContext::getKlassInitCode( std::string name )
-{
-    return classInitCode[name];
-}
+KlassInitCodeAssign& CodeGenContext::getKlassInitCode(std::string name) { return classInitCode[name]; }
 
 llvm::Type* CodeGenContext::getGenericIntegerType()
 {
 #if defined(UseInt64)
-    return Type::getInt64Ty( getGlobalContext() );
+   return Type::getInt64Ty(getGlobalContext());
 #else
-    return Type::getInt32Ty( getGlobalContext() );
+   return Type::getInt32Ty(getGlobalContext());
 #endif
 }
 
-bool CodeGenContext::preProcessing( Block& root )
+bool CodeGenContext::preProcessing(Block& root)
 {
    VisitorSyntaxCheck visitor;
-   root.Accept( visitor );
+   root.Accept(visitor);
    return !visitor.hasErrors();
 }
-
-
-
 }
--- CodeGenContext.h
+++ CodeGenContext.h
@@ -8,7 +8,7 @@
 #include "config.h"
 
 #if defined(_MSC_VER)
-#pragma warning( push , 0 )
+#pragma warning(push, 0)
 #endif
 
 #include "llvm/IR/Module.h"
@@ -28,18 +28,18 @@
 #include <llvm/Support/ManagedStatic.h>
 
 #if defined(_MSC_VER)
-#pragma warning( pop )
+#pragma warning(pop)
 #endif
 
 #include "AstNode.h"
 
-namespace liquid {
-    
+namespace liquid
+{
 ///< Used to keep track the context of a code block.
 enum class ScopeType {
-    Class,
-    FunctionDeclaration,
-    CodeBlock,
+   Class,
+   FunctionDeclaration,
+   CodeBlock,
 };
 
 ///< Maps a variable name to its alloca instruction.
@@ -55,76 +55,82 @@ using KlassInitCodeAssign = std::set<Assignment*>;
 ///< Maps the init code to the class name.
 using KlassInitCode = std::map<std::string, KlassInitCodeAssign>;
 
-///< context of a scoped code block of expressions. 
-class CodeGenBlock {
-    llvm::BasicBlock* bblock;
-    ValueNames locals;
-    VariableTypeMap types;
-public:
-    CodeGenBlock(llvm::BasicBlock* bb) { bblock = bb;}
-    ~CodeGenBlock() { }
-    void setCodeBlock(llvm::BasicBlock* bb) {bblock = bb;}
-    llvm::BasicBlock* currentBlock() {return bblock;}
-    ValueNames& getValueNames() {return locals;}
-    VariableTypeMap& getTypeMap() {return types;}
+///< context of a scoped code block of expressions.
+class CodeGenBlock
+{
+   llvm::BasicBlock* bblock;
+   ValueNames        locals;
+   VariableTypeMap   types;
+
+ public:
+   CodeGenBlock(llvm::BasicBlock* bb) { bblock = bb; }
+   ~CodeGenBlock() {}
+   void              setCodeBlock(llvm::BasicBlock* bb) { bblock = bb; }
+   llvm::BasicBlock* currentBlock() { return bblock; }
+   ValueNames&       getValueNames() { return locals; }
+   VariableTypeMap&  getTypeMap() { return types; }
 };
 
 ///! The context of the current compiling process.
-class CodeGenContext {
-    std::list<CodeGenBlock *> codeBlocks; ///< List of all code blocks
-    CodeGenBlock* self = nullptr;                     ///< The current code block.
-    std::string klassName;                  ///< The current class definition block
-    llvm::Function *mainFunction = nullptr;           ///< main function 
-    llvm::Module *module = nullptr;                   ///< llvm module ...
-    llvm::LLVMContext llvmContext;          ///< and context
-    KlassAttributes classAttributes;        ///< List of attributes for the current class being processed
-    KlassInitCode classInitCode;
-    int errors = 0;                         ///< Count of errors while code gen.
-    ScopeType currentScopeType = ScopeType::CodeBlock;
-    std::ostream& outs;
-    void setCurrentBlock(llvm::BasicBlock *block) {
-        codeBlocks.front()->setCodeBlock(block);
-    }
-    void setupBuiltIns();
-public:
-    llvm::Value* varStruct = nullptr;  ///< Hold the alloc of the structure variable (class object). TODO move it to a better place.
-    bool verbose = false;
-    bool debug = false;
+class CodeGenContext
+{
+   std::list<CodeGenBlock*> codeBlocks;             ///< List of all code blocks
+   CodeGenBlock*            self = nullptr;         ///< The current code block.
+   std::string              klassName;              ///< The current class definition block
+   llvm::Function*          mainFunction = nullptr; ///< main function
+   llvm::Module*            module       = nullptr; ///< llvm module ...
+   llvm::LLVMContext        llvmContext;            ///< and context
+   KlassAttributes          classAttributes;        ///< List of attributes for the current class being processed
+   KlassInitCode            classInitCode;
+   int                      errors           = 0; ///< Count of errors while code gen.
+   ScopeType                currentScopeType = ScopeType::CodeBlock;
+   std::ostream&            outs;
+   void                     setCurrentBlock(llvm::BasicBlock* block) { codeBlocks.front()->setCodeBlock(block); }
+   struct buildin_info_t {
+      llvm::Function* f = nullptr;
+      void*           addr = nullptr;
+   };
+   std::vector<buildin_info_t> builtins;
+   void                         setupBuiltIns();
 
-    CodeGenContext(std::ostream& outs);
-    ~CodeGenContext() { llvm::llvm_shutdown(); }
-        
-    llvm::Module * getModule() {return module;}
-    llvm::LLVMContext& getGlobalContext() {return llvmContext;}
-    void newScope(llvm::BasicBlock* bb = nullptr, ScopeType scopeType = ScopeType::CodeBlock) ;
-    void endScope();
-    ScopeType getScopeType() { return currentScopeType;}
-    void setInsertPoint(llvm::BasicBlock* bblock) {setCurrentBlock(bblock);}
-    llvm::BasicBlock* getInsertPoint() {return currentBlock();}
-    bool generateCode(class Block& root);
-    llvm::GenericValue runCode();
-    void printCodeGeneration(class Block& root, std::ostream& outs);
-    ValueNames& locals() { return codeBlocks.front()->getValueNames(); }
-    void setVarType(std::string varType, std::string varName) {codeBlocks.front()->getTypeMap()[varName] = varType;}
-    std::string getType(std::string varName) ;
-    llvm::AllocaInst* findVariable(std::string varName);
-    void deleteVariable(std::string varName);
-    void renameVariable(std::string oldVarName, std::string newVarName);
+ public:
+   llvm::Value* varStruct = nullptr; ///< Hold the alloc of the structure variable (class object). TODO move it to a better place.
+   bool         verbose   = false;
+   bool         debug     = false;
 
-    llvm::BasicBlock *currentBlock() { return codeBlocks.front()->currentBlock(); }
-    void optimize();
-    void newKlass(std::string name);
-    void endKlass();
-    void klassAddVariableAccess(std::string name, int index);
-    llvm::Instruction* getKlassVarAccessInst(std::string klass, std::string name, llvm::AllocaInst* this_ptr);
-    std::string getKlassName() {return klassName;}
-    llvm::Type* typeOf(const class Identifier& type);
-    llvm::Type* typeOf(const std::string name);
-    void addKlassInitCode( std::string name, Assignment* assign );
-    KlassInitCodeAssign& getKlassInitCode( std::string name );
-    llvm::Type* getGenericIntegerType();
-    bool preProcessing( class Block& root );
-    void addError() { ++errors; }
+   CodeGenContext(std::ostream& outs);
+   ~CodeGenContext() { llvm::llvm_shutdown(); }
+
+   llvm::Module*      getModule() { return module; }
+   llvm::LLVMContext& getGlobalContext() { return llvmContext; }
+   void               newScope(llvm::BasicBlock* bb = nullptr, ScopeType scopeType = ScopeType::CodeBlock);
+   void               endScope();
+   ScopeType          getScopeType() { return currentScopeType; }
+   void               setInsertPoint(llvm::BasicBlock* bblock) { setCurrentBlock(bblock); }
+   llvm::BasicBlock*  getInsertPoint() { return currentBlock(); }
+   bool               generateCode(class Block& root);
+   llvm::GenericValue runCode();
+   void               printCodeGeneration(class Block& root, std::ostream& outs);
+   ValueNames&        locals() { return codeBlocks.front()->getValueNames(); }
+   void               setVarType(std::string varType, std::string varName) { codeBlocks.front()->getTypeMap()[varName] = varType; }
+   std::string        getType(std::string varName);
+   llvm::AllocaInst*  findVariable(std::string varName);
+   void               deleteVariable(std::string varName);
+   void               renameVariable(std::string oldVarName, std::string newVarName);
+
+   llvm::BasicBlock*    currentBlock() { return codeBlocks.front()->currentBlock(); }
+   void                 optimize();
+   void                 newKlass(std::string name);
+   void                 endKlass();
+   void                 klassAddVariableAccess(std::string name, int index);
+   llvm::Instruction*   getKlassVarAccessInst(std::string klass, std::string name, llvm::AllocaInst* this_ptr);
+   std::string          getKlassName() { return klassName; }
+   llvm::Type*          typeOf(const class Identifier& type);
+   llvm::Type*          typeOf(const std::string name);
+   void                 addKlassInitCode(std::string name, Assignment* assign);
+   KlassInitCodeAssign& getKlassInitCode(std::string name);
+   llvm::Type*          getGenericIntegerType();
+   bool                 preProcessing(class Block& root);
+   void                 addError() { ++errors; }
 };
-
 }
